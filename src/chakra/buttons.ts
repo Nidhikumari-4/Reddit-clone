8bfdb0a4881527fd7be3b7259dacdcb4baee65cf
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-02-05 20:43:31 +0530

    Added Navbar Components

---
+++ src/chakra/buttons.ts
@@ -0,0 +1,42 @@
+import { ComponentStyleConfig } from "@chakra-ui/theme";
+
+export const Button: ComponentStyleConfig = {
+  baseStyle: {
+    borderRadius: "60px",
+    fontWeight: "700",
+    fontSize: "10pt",
+    _focus: {
+      boxShadow: "none",
+    },
+  },
+  sizes: {
+    sm: {
+      fontSize: "8pt",
+    },
+    md: {
+      fontSize: "10pt",
+    },
+  },
+  variants: {
+    solid: {
+      bg: "blue.500",
+      color: "white",
+    },
+    _hover: {
+      bg: "blue.400",
+    },
+    outline: {
+      color: "blue.500",
+      border: "1px solid",
+      borderColor: "blue.500",
+    },
+    oauth: {
+      height: "34px",
+      border: "1px solid",
+      borderColor: "gray.300",
+      _hover: {
+        bg: "gray.50",
+      },
+    },
+  },
+};
