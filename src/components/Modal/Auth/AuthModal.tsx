abd62259878be3a60aa3f0cdb3c09e015ce7311d
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-02-06 20:20:28 +0530

    Added authModal and made login and sign up buttons dynamic

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,41 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import React from "react";
+import { useRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalStae] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalStae((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            {modalState.view === "login" && "Log In"}
+            {modalState.view === "signup" && "Sign Up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AuthModal;
