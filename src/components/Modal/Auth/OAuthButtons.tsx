3f788cb4c40b35bd1ca70be8c2924715f7eee197
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-02-09 14:33:48 +0530

    added o auth

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,50 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
-    <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
-        <Image src="/assets/googlelogo.png" height="20px" mr={4} />
-        Continue with Google
+    <Flex mb={4}>
+      <Button
+        variant="oauth"
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
+        <Image src="/assets/googlelogo.png" height="20px" />
       </Button>
-      <Button variant="oauth">Some other Provider</Button>
+      <Button
+        variant="oauth"
+        // isLoading={loading}
+        // onClick={() => signInWithGoogle()}
+      >
+        <Image src="/assets/microsoftlogo.png" height="20px" />
+      </Button>
+      <Button variant="oauth">
+        <Image src="/assets/githublogo.png" height="20px" />
+      </Button>
+      <Button
+        variant="oauth"
+        // isLoading={loading}
+        // onClick={() => signInWithGoogle()}
+      >
+        <Image src="/assets/facebooklogo.png" height="20px" />
+      </Button>
+      <Button
+        variant="oauth"
+        // isLoading={loading}
+        // onClick={() => signInWithGoogle()}
+      >
+        <Image src="/assets/twitterlogo.png" height="20px" />
+      </Button>
+
+      {/* {error && (
+        <Text display="flex"  alignItems="center" justifyContent="center">
+          {error.message}
+        </Text>
+      )} */}
     </Flex>
   );
 };
