c77a069013ea62bf4b7f1170691cdd5a505d3619
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-02-11 13:35:08 +0530

    Added Login error

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,4 +1,7 @@
 export const FIREBASE_ERRORS = {
   "Firebase: Error (auth/email-already-in-use).":
     "The email is already in use by another user.",
+
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password",
 };
