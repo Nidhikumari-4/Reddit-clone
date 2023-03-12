c0d505187a63aec0690b8329bfcb2e16f3254fd2
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-03-13 00:34:14 +0530

    +

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Box,
   Menu,
   Text,
   MenuButton,
@@ -8,59 +9,67 @@ import {
   Icon,
   Image,
 } from "@chakra-ui/react";
-import { TiHome } from "react-icons/ti";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import Communities from "./Communities";
-import useDirectory from "@/hooks/useDirectory";
+import useDirectory from "../../../hooks/useDirectory";
 
-const UserMenu: React.FC = () => {
+const Directory: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
   const { directoryState, toggleMenuOpen } = useDirectory();
 
   return (
     <Menu isOpen={directoryState.isOpen}>
-      <MenuButton
-        cursor="pointer"
-        padding="0px 6px"
-        borderRadius={4}
-        mr={2}
-        ml={{ base: 0, md: 2 }}
-        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-        onClick={toggleMenuOpen}
-      >
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          width={{ base: "auto", lg: "200px" }}
+      <>
+        <MenuButton
+          cursor="pointer"
+          padding="0px 6px"
+          borderRadius="4px"
+          mr={2}
+          ml={{ base: 0, md: 2 }}
+          _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+          onClick={toggleMenuOpen}
         >
-          <Flex alignItems="center">
-            {directoryState.selectedMenuItem.imageURL ? (
-              <Image
-                src={directoryState.selectedMenuItem.imageURL}
-                borderRadius="full"
-                boxSize="24px"
-                mr={2}
-              />
-            ) : (
-              <Icon
-                fontSize={24}
-                mr={{ base: 1, md: 2 }}
-                as={directoryState.selectedMenuItem.icon}
-                color={directoryState.selectedMenuItem.iconColor}
-              />
-            )}
-            <Flex display={{ base: "none", lg: "flex" }}>
-              <Text fontWeight={600} fontSize="10pt">
-                {directoryState.selectedMenuItem.displayText}
-              </Text>
+          <Flex
+            alignItems="center"
+            justifyContent="space-between"
+            width={{ base: "auto", lg: "200px" }}
+          >
+            <Flex alignItems="center">
+              {directoryState.selectedMenuItem.imageURL ? (
+                <Image
+                  borderRadius="full"
+                  boxSize="24px"
+                  src={directoryState.selectedMenuItem.imageURL}
+                  mr={2}
+                />
+              ) : (
+                <Icon
+                  fontSize={24}
+                  mr={{ base: 1, md: 2 }}
+                  color={directoryState.selectedMenuItem.iconColor}
+                  as={directoryState.selectedMenuItem.icon}
+                />
+              )}
+              <Box
+                display={{ base: "none", lg: "flex" }}
+                flexDirection="column"
+                fontSize="10pt"
+              >
+                <Text fontWeight={600}>
+                  {directoryState.selectedMenuItem.displayText}
+                </Text>
+              </Box>
             </Flex>
+            <ChevronDownIcon color="gray.500" />
           </Flex>
-          <ChevronDownIcon />
-        </Flex>
-      </MenuButton>
-      <MenuList>
-        <Communities />
-      </MenuList>
+        </MenuButton>
+        <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+          <Communities />
+        </MenuList>
+      </>
     </Menu>
   );
 };
-export default UserMenu;
+export default Directory;
